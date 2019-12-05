0bec02a4bf478bf7ed8e9e8fb4df0b4950c01c3c
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2019-12-05 16:47:01 +0100

    5.4 connect to firebase

--- components/Chat.js
+++ components/Chat.js
@@ -4,44 +4,141 @@ import { StyleSheet, Text, View, TextInput, Platform } from "react-native";
 //Android only - correct screen keyboard display
 import KeyboardSpacer from "react-native-keyboard-spacer";
 
+//establish firebase connection
+const firebase = require("firebase");
+require("firebase/firestore");
+
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyBvmNRw4RtZNTZcqesOpBZO6rLd690fnEI",
+        authDomain: "hello-chat-cb7a5.firebaseapp.com",
+        databaseURL: "https://hello-chat-cb7a5.firebaseio.com",
+        projectId: "hello-chat-cb7a5",
+        storageBucket: "hello-chat-cb7a5.appspot.com",
+        messagingSenderId: "973755903581",
+        appId: "1:973755903581:web:cfe5b4377b70e7a3ac27b4"
+      });
+    }
+
+    //create reference to firestore collection
+    this.referenceUser = null;
+    this.referenceMessages = firebase.firestore().collection("messages");
+
+    //set state to be array of messages
+    this.state = {
+      messages: [],
+      uid: "",
+      loggedInText: "Please wait while we log you in",
+      user: {
+        _id: "",
+        name: "",
+        avatar: ""
+      }
+    };
   }
-  //set state to be array of messages
-  state = {
-    messages: []
+
+  //add username to navigation title
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.state.params.name
+    };
   };
 
-  componentDidMount() {
+  /*
+   * update messages array in state
+   * @function onCollectionUpdate
+   * @param {string} _id
+   * @param {string} text
+   * @param {date} createdAt
+   * @param {string} user
+   */
+
+  onCollectionUpdate = querySnapshot => {
+    const messages = [];
+    //go through each message document
+    querySnapshot.forEach(doc => {
+      //get QuerySnapshot data
+      const data = doc.data();
+
+      //push to messages array
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user
+      });
+    });
+    //set messages array as state
     this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any"
-          }
-        },
-        {
-          _id: 2,
-          text:
-            this.props.navigation.state.params.name + " has joined the chat.",
-          createdAt: new Date(),
-          system: true
-        }
-      ]
+      messages
+    });
+  };
+
+  /*
+   * add message to firebase database
+   * @function addMessage
+   * @param {number} _id
+   * @param {string} text
+   * @param {date} createdAt
+   * @param {string} user
+   */
+  addMessage() {
+    const message = this.state.messages[0];
+
+    this.referenceMessages.add({
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: this.state.user
     });
   }
 
-  //append new messages to messages array in state
+  //append new messages to messages array in state & database
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages)
-    }));
+    this.setState(
+      previousState => ({
+        messages: GiftedChat.append(previousState.messages, messages)
+      }),
+      () => {
+        this.addMessage();
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
+      if (!user) {
+        try {
+          await firebase.auth().signInAnonymously();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      //update user state with currently active user data
+      this.setState({
+        uid: user.uid,
+        loggedInText: "Hey there",
+        user: {
+          _id: user.uid,
+          name: this.props.navigation.state.params.name
+        }
+      });
+    });
+    this.referenceUser = firebase
+      .firestore()
+      .collection("messages")
+      .orderBy("createdAt", "desc");
+    this.unsubscribeUser = this.referenceUser.onSnapshot(
+      this.onCollectionUpdate
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeUser();
+    this.authUnsubscribe();
   }
 
   //customise appearance of message speech bubbles
@@ -58,13 +155,6 @@ export default class Chat extends React.Component {
     );
   }
 
-  //add username to navigation title
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.state.params.name
-    };
-  };
-
   render() {
     return (
       <View
@@ -79,9 +169,7 @@ export default class Chat extends React.Component {
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
-          user={{
-            _id: 1
-          }}
+          user={this.state.user}
         />
         {Platform.OS === "android" ? <KeyboardSpacer /> : null}
       </View>
